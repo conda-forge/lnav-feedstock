ffca60f2bff7f1c460ce5edd20d8b0d9e20d31e1
UENO, M. <eunosium-1128@live.jp>
2025-07-25 16:40:52 +0900

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,30 +4,6 @@ set -exo pipefail
 
 export CXXFLAGS="${CXXFLAGS} -fpermissive -std=c++17"
 
-# `ptsname_r` compatibility for macOS
-if [[ "${target_platform}" == "osx-"* ]]; then
-    # Add necessary includes and compatibility function at the top of the file
-    sed -i.bak '1i\
-#if defined(__APPLE__)\
-#include <stdlib.h>\
-#include <unistd.h>\
-#include <string.h>\
-static int ptsname_compat(int fd, char* buf, size_t buflen) {\
-    char* name = ptsname(fd);\
-    if (!name) return -1;\
-    if (strlen(name) >= buflen) return -1;\
-    strcpy(buf, name);\
-    return 0;\
-}\
-#else\
-#define ptsname_compat ptsname_r\
-#endif\
-' src/readline_curses.cc
-
-    # Replace ptsname_r calls with ptsname_compat
-    sed -i.bak 's/ptsname_r(/ptsname_compat(/g' src/readline_curses.cc
-fi
-
 # Build prqlc-c library in advance
 cd src/third-party/prqlc-c
 cargo-bundle-licenses --format yaml --output ${SRC_DIR}/THIRDPARTY.yml
